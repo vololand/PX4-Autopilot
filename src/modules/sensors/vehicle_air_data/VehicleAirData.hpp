b120633b6aa1ac7011d81c712488a31fa746c7de
Marco Hauswirth <marco.hauswirth@auterion.com>
2025-01-20 18:04:13 +0100

    remove temp field from airspeed.msg, adjust temp selection

--- src/modules/sensors/vehicle_air_data/VehicleAirData.hpp
+++ src/modules/sensors/vehicle_air_data/VehicleAirData.hpp
@@ -75,7 +75,7 @@ public:
 private:
 	void Run() override;
 
-	void AirTemperatureUpdate();
+	void AirTemperatureUpdate(float &temperature);
 	void CheckFailover(const hrt_abstime &time_now_us);
 	bool ParametersUpdate(bool force = false);
 	void UpdateStatus();
@@ -123,8 +123,6 @@ private:
 	uint8_t _priority[MAX_SENSOR_COUNT] {};
 
 	int8_t _selected_sensor_sub_index{-1};
-
-	float _air_temperature_celsius{20.f}; // initialize with typical 20degC ambient temperature
 
 	DEFINE_PARAMETERS(
 		(ParamFloat<px4::params::SENS_BARO_QNH>) _param_sens_baro_qnh,
